ad0cbb26625901b525c580f868bc1048aba83bd7
Matiboux <matiboux@gmail.com>
2024-11-12 23:01:57 +0100

    Add tests for commands

---
+++ test/test/command/test_default.py
@@ -0,0 +1,14 @@
+from test.src.TestDirContext import TestDirContext
+from test.src.format_dockerx_stdout import format_dockerx_stdout
+
+def test_default(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'ubuntu',
+		)
+		# Default is to run the image's default command
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir ubuntu',
+			),
+		)

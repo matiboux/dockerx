c1925c48e7545c196f29aa39f1eea22301dbf071
Matiboux <matiboux@gmail.com>
2024-11-12 22:51:33 +0100

    Add tests with options

---
+++ test/test/options/test_simple.py
@@ -0,0 +1,26 @@
+from test.src.TestDirContext import TestDirContext
+from test.src.format_dockerx_stdout import format_dockerx_stdout
+
+def test_short_option(file = __file__):
+	# Invalid usage
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'ubuntu', '-q', '--',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir -q ubuntu',
+			),
+		)
+
+def test_long_option(file = __file__):
+	# Invalid usage
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'ubuntu', '-e', 'VAR=VALUE', '--',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir -e VAR=VALUE ubuntu',
+			),
+		)

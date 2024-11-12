8647e835f6fd67c4cd2b36348db85f17c7275f23
Mati <matiboux@gmail.com>
2024-11-12 22:53:35 +0100

    Fix & Improve docker options (#3)

---
+++ test/test/image/test_simple.py
@@ -0,0 +1,35 @@
+from test.src.TestDirContext import TestDirContext
+from test.src.format_dockerx_stdout import format_dockerx_stdout
+
+def test_hello_world(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'hello-world',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir hello-world',
+			),
+		)
+
+def test_ubuntu(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'ubuntu',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir ubuntu',
+			),
+		)
+
+def test_nginx(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'nginx',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir nginx',
+			),
+		)

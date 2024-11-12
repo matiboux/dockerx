69e0405f3291232909aa8870e15f822d1f547717
Matiboux <matiboux@gmail.com>
2024-11-12 22:57:30 +0100

    Refactor tests with options

--- test/test/options/test_multiple.py
+++ test/test/options/test_multiple.py
@@ -2,7 +2,6 @@ from test.src.TestDirContext import TestDirContext
 from test.src.format_dockerx_stdout import format_dockerx_stdout
 
 def test_multiple_options(file = __file__):
-	# Invalid usage
 	with TestDirContext(file) as ctx:
 		dockerx = ctx.run_dockerx(
 			'-n', 'ubuntu', '-q', '-e', 'VAR=VALUE', '--',

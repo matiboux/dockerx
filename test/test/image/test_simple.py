33addc1768004c86bb1391ab84f5758b050ddd8f
Matiboux <matiboux@gmail.com>
2024-11-12 22:41:43 +0100

    Add more tests with image

--- test/test/image/test_simple.py
+++ test/test/image/test_simple.py
@@ -1,7 +1,18 @@
 from test.src.TestDirContext import TestDirContext
 from test.src.format_dockerx_stdout import format_dockerx_stdout
 
-def test_simple(file = __file__):
+def test_hello_world(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'hello-world',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir hello-world',
+			),
+		)
+
+def test_ubuntu(file = __file__):
 	with TestDirContext(file) as ctx:
 		dockerx = ctx.run_dockerx(
 			'-n', 'ubuntu',
@@ -9,5 +20,16 @@ def test_simple(file = __file__):
 		dockerx.assert_context_ok(
 			format_dockerx_stdout(
 				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir ubuntu',
+			),
+		)
+
+def test_nginx(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'nginx',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir nginx',
 			),
 		)

15b4c6c02754ad0d0af5a063f0e988220862cd4c
Matiboux <matiboux@gmail.com>
2024-11-13 01:48:18 +0100

    Add command sh test

--- test/test/command/test_simple.py
+++ test/test/command/test_simple.py
@@ -1,6 +1,17 @@
 from test.src.TestDirContext import TestDirContext
 from test.src.format_dockerx_stdout import format_dockerx_stdout
 
+def test_sh(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'ubuntu', 'sh',
+		)
+		dockerx.assert_context_ok(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir ubuntu sh',
+			),
+		)
+
 def test_ls(file = __file__):
 	with TestDirContext(file) as ctx:
 		dockerx = ctx.run_dockerx(

db2a21dc4658ad2a2230344396277e84938632d2
Matiboux <matiboux@gmail.com>
2024-11-13 01:59:03 +0100

    Test command presets with spaces

---
+++ test/test/command_presets/test_spaces.py
@@ -0,0 +1,24 @@
+from test.src.TestDirContext import TestDirContext
+from test.src.format_dockerx_stdout import format_dockerx_stdout
+
+def test_space(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'ubuntu', '@yes sir',
+		)
+		# Default is to run the image's default command
+		dockerx.assert_context_error(
+			b'Error: Unknown command preset \'@yes sir\'\n'
+		)
+
+def test_escaped_space(file = __file__):
+	with TestDirContext(file) as ctx:
+		dockerx = ctx.run_dockerx(
+			'-n', 'ubuntu', '@@yes sir',
+		)
+		# Default is to run the image's default command
+		dockerx.assert_context_error(
+			format_dockerx_stdout(
+				b'docker run -it --rm -v \'' + ctx.cwd.encode() + b':/workdir\' -w /workdir ubuntu \'@yes sir\'',
+			),
+		)

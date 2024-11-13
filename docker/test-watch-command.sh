9068fe3ce5e2d6fed74f9489e8500887231be9a1
Matiboux <matiboux@gmail.com>
2024-11-13 17:28:46 +0100

    Refactor Pytest watch runner argument

--- docker/test-watch-command.sh
+++ docker/test-watch-command.sh
@@ -6,5 +6,12 @@ if [ -n "$TESTMON_DATAFILE" ]; then
 	mkdir -p "$(dirname $TESTMON_DATAFILE)"
 fi
 
-# Run Pytest using Pytest Watch & Testmon with coverage
-pytest-watch --runner "pytest --testmon --cov --cov-report term-missing:skip-covered --cov-report json:cov.json -vv"
+# Run Pytest with Testmon and coverage
+PYTEST_RUNNER="\
+pytest --testmon --cov -vv \
+--cov-report term-missing:skip-covered \
+--cov-report json:cov.json \
+"
+
+# Run Pytest Watch using configured Pytest runner
+pytest-watch --runner "$PYTEST_RUNNER"

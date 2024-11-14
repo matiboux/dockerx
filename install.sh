126668cea579a3bacab6dcf3f4256d526f205ae4
Matiboux <matiboux@gmail.com>
2024-11-14 05:06:21 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -7,69 +7,106 @@
 # Copyright (c) 2024 Matiboux
 # This project is not affiliated with Docker, Inc.
 
-if [ "$#" -gt 0 ]; then
+ERROR_CODE=''
+DOCKERX_PARSE_ARGUMENTS='true'
 
-	if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-		echo "Usage: install.sh [install_dir] [version]"
-		echo "  install_dir: Installation directory (default: /usr/local/bin)"
-		echo "  version: Specific version to install (default: HEAD)"
-		exit 0
+# Parse options arguments
+while [ "$DOCKERX_PARSE_ARGUMENTS" = 'true' ] && [ "$#" -gt 0 ]; do
+
+	case "$1" in
+
+		'--help' | '-h' )
+			DOCKERX_PRINT_HELP='true'
+			shift
+			;;
+
+		'--install-dir' | '-i' )
+			shift
+			# Get installation directory argument
+			if [ -z "$1" ]; then
+				echo 'Error: Missing installation directory.' >&2
+				DOCKERX_PRINT_HELP='true'
+				ERROR_CODE=1
+				# Stop parsing arguments
+				DOCKERX_PARSE_ARGUMENTS='false'
+				break
+			fi
+			DOCKERX_INSTALL_DIR="$1"
+			shift
+			;;
+
+		* )
+			# Unknown option, maybe first argument
+			# Stop parsing options
+			break
+			;;
+
+	esac
+
+done
+
+if [ "$DOCKERX_PARSE_ARGUMENTS" = 'true' ]; then
+	# Parse positional arguments
+
+	# Parse install tag optional argument
+	if [ "$#" -gt 0 ]; then
+		DOCKERX_INSTALL_TAG="$1"
+		shift
 	fi
 
-	if [ "$1" = "--install-dir" ] || [ "$1" = "-i" ]; then
-		# Installation directory argument is provided
-		shift
-		DOCKERX_INSTALL_DIR="$1"
-		shift
-		if [ -z "$DOCKERX_INSTALL_DIR" ]; then
-			echo "Error: Missing installation directory." >&2
-			exit 1
-		fi
-	fi
+fi
 
-	if [ -n "$1" ]; then
-		# Version argument is provided
-		DOCKERX_REQUIRED_TAG="v$1"
-		shift
-	fi
-
+if [ "$DOCKERX_PRINT_HELP" = 'true' ]; then
+	# Print help & exit
+	echo "Usage: $0 [options] [tag]"
+	echo ''
+	echo 'Options:'
+	echo '  --help, -h         Display this help message'
+	echo '  --install-dir, -i  Installation directory (defaults to /usr/local/bin)'
+	echo ''
+	echo 'Arguments:'
+	echo '  tag  DockerX tag/version to install (defaults to HEAD)'
+	exit ${ERROR_CODE:-0}
 fi
 
 # Get installation directory
+INSTALL_DIR='/usr/local/bin' # Default installation directory
 if [ -n "$DOCKERX_INSTALL_DIR" ]; then
 	# Use from argument or environment variable
 	INSTALL_DIR="$DOCKERX_INSTALL_DIR"
-else
-	# Default installation directory
-	INSTALL_DIR='/usr/local/bin'
 fi
 
-# Get required tag to install
-if [ -n "$DOCKERX_REQUIRED_TAG" ]; then
+# Get install tag
+INSTALL_TAG='HEAD' # Default required tag
+if [ -n "$DOCKERX_INSTALL_TAG" ]; then
 	# Use from argument or environment variable
-	REQUIRED_TAG="$DOCKERX_REQUIRED_TAG"
-else
-	# Default required tag
-	REQUIRED_TAG="HEAD"
+	INSTALL_TAG="$DOCKERX_INSTALL_TAG"
 fi
 
 # Check that docker is installed
 docker --help > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Error: Docker is not installed." >&2
+	echo 'Error: Docker is not installed.' >&2
 	exit 1
 fi
 
-curl -fsSL https://raw.githubusercontent.com/matiboux/dockerx/$REQUIRED_TAG/dockerx -o "$INSTALL_DIR/dockerx"
+# Check that docker compose is installed
+docker compose --help > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-	echo "Error: DockerX installation failed." >&2
+	echo 'Error: Docker compose is not installed.' >&2
+	exit 1
+fi
+
+curl -fsSL "https://raw.githubusercontent.com/matiboux/dockerx/$INSTALL_TAG/dockerx" -o "$INSTALL_DIR/dockerx"
+if [ $? -ne 0 ]; then
+	echo 'Error: DockerX installation failed.' >&2
 	exit 1
 fi
 
 chmod +x "$INSTALL_DIR/dockerx"
 if [ $? -ne 0 ]; then
-	echo "Error: Failed to add execute permission." >&2
+	echo 'Error: Failed to add execute permission.' >&2
 	exit 1
 fi
 
-echo "DockerX ($REQUIRED_TAG) installed successfully at '$INSTALL_DIR/dockerx'!"
+echo "DockerX ($INSTALL_TAG) installed successfully at '$INSTALL_DIR/dockerx'!"
